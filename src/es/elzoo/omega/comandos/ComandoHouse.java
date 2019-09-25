c353400d09d293598a6f92545539cbdf74e65832
Chistian95 <kriskorral59@gmail.com>
2019-09-26 01:55:53 +0200

    Comando /house info

--- src/es/elzoo/omega/comandos/ComandoHouse.java
+++ src/es/elzoo/omega/comandos/ComandoHouse.java
@@ -10,8 +10,11 @@ import org.bukkit.entity.Player;
 
 import es.elzoo.omega.Mensajes;
 import es.elzoo.omega.Permisos;
+import es.elzoo.omega.casa.Casa;
 import es.elzoo.omega.casa.CasaAsistenteCrear;
 import es.elzoo.omega.casa.Clase;
+import es.elzoo.omega.casa.gui.GUICasaGuest;
+import es.elzoo.omega.casa.gui.GUICasaOwner;
 
 public class ComandoHouse implements CommandExecutor {
 	@Override
@@ -29,11 +32,20 @@ public class ComandoHouse implements CommandExecutor {
 			crearCasa(player, args);
 		} else if(args[0].equalsIgnoreCase("cancel")) {
 			cancelarAsistente(player);
-		} else if(args[0].equalsIgnoreCase("class")) {
+		} else if(args[0].equalsIgnoreCase("createClass")) {
 			comandoClass(player, args);
+		} else if(args[0].equalsIgnoreCase("info")) {
+			info(player, args);
 		}
 		
 		return true;
+	}
+	
+	private static void mostrarAyuda(Player player) {
+		player.sendMessage(ChatColor.GRAY+"/house create - Starts the assistant to add a new house.");
+		player.sendMessage(ChatColor.GRAY+"/house cancel - Cancels the current assistant.");
+		player.sendMessage(ChatColor.GRAY+"/house createClass <id> <price> <chests> - Cancels the current assistant.");
+		player.sendMessage(ChatColor.GRAY+"/house info <class> <number> - Shows the info of the selected house.");
 	}
 	
 	private static void crearCasa(Player player, String[] args) {
@@ -107,11 +119,54 @@ public class ComandoHouse implements CommandExecutor {
 		
 		new Clase(id, price, chests);
 		player.sendMessage(ChatColor.GREEN + "Class created.");
-	}
+	}	
 	
-	private static void mostrarAyuda(Player player) {
-		player.sendMessage(ChatColor.GRAY+"/house create - Starts the assistant to add a new house.");
-		player.sendMessage(ChatColor.GRAY+"/house cancel - Cancels the current assistant.");
-		player.sendMessage(ChatColor.GRAY+"/house createClass <id> <price> <chests> - Cancels the current assistant.");
+	private static void info(Player player, String[] args) {
+		if(args.length != 3) {
+			player.sendMessage(ChatColor.GRAY+"/house info <class> <number> - Shows the info of the selected house.");
+			return;
+		}
+		
+		int classId = 0;
+		try {
+			classId = Integer.parseInt(args[1]);
+		} catch(Exception e) {
+			classId = 0;			
+		}
+		if(classId <= 0) {
+			player.sendMessage(ChatColor.RED + "Error parsing the class. It must be a number bigger than 0.");
+			return;
+		}
+		
+		int houseNumber = 0;
+		try {
+			houseNumber = Integer.parseInt(args[3]);
+		} catch(Exception e) {
+			houseNumber = 0;			
+		}
+		if(houseNumber <= 0) {
+			player.sendMessage(ChatColor.RED + "Error parsing the house number. It must be a number bigger than 0.");
+			return;
+		}
+		
+		Optional<Clase> clase = Clase.getClaseById(classId);
+		if(!clase.isPresent()) {
+			player.sendMessage(ChatColor.RED + "Could not find a class with that number.");
+			return;
+		}
+		
+		Optional<Casa> casa = Casa.getCasaByClaseYNumero(clase.get(), houseNumber);
+		if(!casa.isPresent()) {
+			player.sendMessage(ChatColor.RED + "Could not find a house by that class and number.");
+			return;
+		}
+		
+		if(casa.get().isOwner(player)) {
+			GUICasaOwner gui = new GUICasaOwner(casa.get());
+			gui.abrir(player);
+		} else {
+			GUICasaGuest gui = new GUICasaGuest(casa.get());
+			gui.abrir(player);
+		}
 	}
 }
